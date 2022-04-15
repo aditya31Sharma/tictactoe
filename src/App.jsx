5a58facfbd1ff341de2db64d547cefdf4f8c79e2
aditya31Sharma <aditya311001sharma@gmail.com>
2022-04-15 11:39:49 +0530

    added sass and other styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-
+import React from 'react';
+import Board from './components/Board';
+import './styles/root.scss';
 const App = () => {
-  return <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
+  return (
+    <div className="app">
+      <h1>TicTacToe</h1>
+      <Board />
+    </div>
+  );
 };
 export default App;
---
+++ src/components/Square.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const Square = () => {
+  return (
+    <button type="button" className='square'>O</button>
+  )
+}
+
+export default Square
